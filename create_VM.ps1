95087afb7b53b18ff36fd733004b329114a330d0
asahi <49680474+asahi0104@users.noreply.github.com>
2021-08-29 17:13:07 +0900

    create vm material

---
+++ create_VM.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module VMware.PowerCLI
+Connect-VIServer -Server 'XXXXX' -User root -Password "XXXXX" -Force
+Start-Sleep 10
+
+$vmname = "vm1"
+
+
+New-VM -Name $vmname -Datastore 'XXXXXX' -NumCpu 2 -DiskGB 20 -MemoryGB 1 -DiskStorageFormat 'thin' -NetworkName 'VM Network' -CD -GuestID 'rhel8_64Guest'
+
+New-AdvancedSetting -Entity $vmname -Name isolation.tools.copy.disable -Value TRUE -Force
+#Disconnect-VIServer -Confirm:$false
